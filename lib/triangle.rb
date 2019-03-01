dc9db6c9894c57fbb6a78ec17dd9a258f5389e79
Tyler Hunt <tyhunt04@gmail.com>
2019-03-01 16:41:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @sides_of_triangle = []
+    @sides_of_triangle << side_1
+    @sides_of_triangle << side_2
+    @sides_of_triangle << side_3
+  end
+
+  def valid?
+    sides_1_2 = @sides_of_triangle[0] + @sides_of_triangle[1]
+    sides_2_3 = @sides_of_triangle[1] + @sides_of_triangle[2]
+    sides_1_3 = @sides_of_triangle[0] + @sides_of_triangle[2]
+
+    if (@sides_of_triangle.none? {|side| side <= 0})  && 
+      (sides_1_2 > @sides_of_triangle[2] && sides_1_3 > @sides_of_triangle[1] && sides_2_3 > @sides_of_triangle[0])
+      true
+    else
+      false
+    end
+  end
 end
